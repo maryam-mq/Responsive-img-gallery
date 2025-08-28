0bb6d2fda65c3c45f82eb5c11eaa45182fead914
maryam-mq <merry.mq.19@gmail.com>
2025-08-28 19:21:16 +0500

    Initial commit - Responsive Image Gallery

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterImages(category) {
+  const items = document.querySelectorAll('.gallery .item');
+  items.forEach(item => {
+    if (category === 'all' || item.classList.contains(category)) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
